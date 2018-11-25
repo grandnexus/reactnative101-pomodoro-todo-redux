41a2762d97686d30282ee8ac2aa11dd2ca187fac
Jason Choo <choo.yansheng@gmail.com>
2018-11-26 00:53:04 +0800

    [Feature] Added dashboard screen

--- pomodoro-todo-completed/src/containers/dashboard/Dashboard.js
+++ pomodoro-todo-completed/src/containers/dashboard/Dashboard.js
@@ -2,26 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-const Dashboard = ({ Layout }) => (
-  <Layout />
+import { addTask, updateTask, deleteTask } from '../../actions/tasks';
+
+const Dashboard = ({ Layout, tasks, addTask, updateTask, deleteTask }) => (
+  <Layout
+    tasks={tasks.tasks}
+    addTask={addTask}
+    updateTask={updateTask}
+    deleteTask={deleteTask}
+  />
 );
 
 Dashboard.propTypes = {
   Layout: PropTypes.func.isRequired,
+  tasks: PropTypes.shape({}).isRequired,
+  addTask: PropTypes.func.isRequired,
+  updateTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired
 };
 
 Dashboard.defaultProps = {};
 
 const mapStateToProps = state => ({
-  // portfolios: state.portfolios || {}
+  tasks: state.tasks || {}
 });
 
 const mapDispatchToProps = {
-  // onFormSubmit: searchAgent,
-  // onFormReset: searchAgentReset
+  addTask,
+  updateTask,
+  deleteTask,
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Dashboard);
--- pomodoro-todo-completed/src/native/components/dashboard/Dashboard.js
+++ pomodoro-todo-completed/src/native/components/dashboard/Dashboard.js
@@ -1,15 +1,105 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
-import { Container, Content } from 'native-base';
-
+import { StyleSheet, StatusBar } from 'react-native';
+import { Container, Content, Header, Left, Button, Icon, Body, Right } from 'native-base';
+import { FontAwesome } from '@expo/vector-icons';
+import Spacer from '../common/Spacer';
+import TodoCheckbox from '../common/TodoCheckbox';
+import { Actions } from 'react-native-router-flux';
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoTasks: props.tasks,
+      newTask: '',
+    }
+  }
+
+  listTodoTasks() {
+    return this.state.todoTasks.map(item => {
+      return (
+        <TodoCheckbox
+          key={item.id}
+          task={item.taskName}
+          checked={item.isChecked}
+          inverted={true}
+          onPress={() => {
+            this.props.updateTask({
+              id: item.id,
+              taskName: item.taskName,
+              isChecked: !item.isChecked,
+              timeCompleted: null,
+            });
+          }}
+          onChangeText={(text) => {
+            this.props.updateTask({
+              id: item.id,
+              taskName: text,
+              isChecked: false,
+              timeCompleted: null
+            })
+          }}
+          onDelete={() =>
+            this.props.deleteTask({
+              id: item.id
+            })
+          }
+        />
+      );
+    });
+  }
+
   render() {
     return (<Container>
-      <Content>
-        <Text>Dashboard</Text>
+      <StatusBar hidden />
+      <Header transparent>
+        <Left>
+          <Button transparent onPress={() => Actions.pop()}>
+            <Icon name='arrow-back' style={styles.leftButtonIcon} />
+          </Button>
+        </Left>
+        <Body />
+        <Right>
+        </Right>
+      </Header>
+      <Content style={styles.content} padder>
+        {this.listTodoTasks()}
+        <TodoCheckbox
+          task={this.state.newTask}
+          isInputField={true}
+          inverted={true}
+          onChangeText={(text) => {
+            this.setState({
+              newTask: text
+            });
+            console.log(text);
+          }}
+          onSubmitEditing={() => {
+            if (this.state.newTask.length > 0) {
+              this.props.addTask({
+                id: this.props.tasks.length,
+                taskName: this.state.newTask,
+                isChecked: false,
+                timeCompleted: null
+              })
+              this.setState({
+                newTask: ''
+              });
+            }
+          }}
+        />
+        <Spacer size={50} />
       </Content>
     </Container>);
   }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    padding: 20
+  },
+  leftButtonIcon: {
+    color: 'black'
+  }
+});
 
 export default Dashboard;
